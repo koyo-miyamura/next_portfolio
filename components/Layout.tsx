06e5b36470d40f1639e42dd619ac4cdb390b069b
koyo-miyamura <nexus9071@gmail.com>
2021-07-23 23:46:57 +0900

    Lint

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -18,26 +18,17 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
     </Head>
     <header className="mt-2 mb-6">
       <nav>
-        <Link href="/">
-          Home
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          About
-        </Link>{' '}
-        |{' '}
+        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
         <a href="https://github.com/koyo-miyamura">
-        <FontAwesomeIcon icon={faGithub} />
+          <FontAwesomeIcon icon={faGithub} />
         </a>{' '}
         |{' '}
         <a href="https://twitter.com/koyomiyamura">
-        <FontAwesomeIcon icon={faTwitter} />
+          <FontAwesomeIcon icon={faTwitter} />
         </a>
       </nav>
     </header>
-    <main className="flex flex-col items-center w-full flex-1 text-center">
-    {children}
-    </main>
+    <main className="flex flex-col items-center w-full flex-1 text-center">{children}</main>
     <footer className="flex items-center justify-center w-full h-24 border-t text-sm">
       © {new Date().getFullYear()} Koyo Miyamura
     </footer>
