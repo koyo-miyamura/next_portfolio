575f802af93bcd609ab5cf9fe0ea6ae14fb7cb55
koyo-miyamura <nexus9071@gmail.com>
2021-07-23 20:57:30 +0900

    eslint噛ませた

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
+import React from 'react'
 import Layout from '../components/Layout'
 
-const IndexPage = () => (
-  <Layout title="Home">
-    <div className="flex justify-center items-center w-full bg-blue-200">
-      <img className="animate__animated animate__fadeIn lg:w-1/2" src="/images/portfolio_top.png" alt="ポートフォリオ" />
-    </div>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout title="Home">
+      <div className="flex justify-center items-center w-full bg-blue-200">
+        <img
+          className="animate__animated animate__fadeIn lg:w-1/2"
+          src="/images/portfolio_top.png"
+          alt="ポートフォリオ"
+        />
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
