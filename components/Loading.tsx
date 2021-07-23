acd4bb6d232c1355ec1c6388f7109f53cb3ea64e
koyo-miyamura <nexus9071@gmail.com>
2021-07-24 02:48:36 +0900

    Loadingコンポーネントにまとめる

---
+++ components/Loading.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import { useRecoilLoading } from '../store/loading'
+import { timer } from '../lib/utils'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const Loading = ({ children }: Props) => {
+  const [isLoaded, setIsLoaded] = useRecoilLoading()
+
+  useEffect(() => {
+    const f = async () => {
+      await timer(2000)
+      setIsLoaded(true)
+    }
+    f()
+  }, [])
+
+  if (!isLoaded) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center bg-white z-50">
+        <img className="animate__animated animate__fadeInDown" src="/images/momizi_icon.png" alt="アイコン" />
+      </div>
+    )
+  }
+
+  return <>{children}</>
+}
+
+export default Loading
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,20 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Layout from '../components/Layout'
-import { useRecoilLoading } from '../store/loading'
-import { timer } from '../lib/utils'
+import Loading from '../components/Loading'
 
 const IndexPage = () => {
-  const [isLoaded, setIsLoaded] = useRecoilLoading()
-
-  useEffect(() => {
-    const f = async () => {
-      await timer(2000)
-      setIsLoaded(true)
-    }
-    f()
-  }, [])
-
-  const Loading = () => (
-    <div className="w-full h-screen flex justify-center items-center bg-white z-50">
-      <img className="animate__animated animate__fadeInDown" src="/images/momizi_icon.png" alt="アイコン" />
-    </div>
-  )
-
-  if (!isLoaded) {
-    return <Loading />
-  }
-
   return (
-    <Layout title="Home">
-      <div className="flex justify-center items-center w-full bg-blue-200">
-        <img
-          className="animate__animated animate__fadeIn lg:w-1/2"
-          src="/images/portfolio_top.png"
-          alt="ポートフォリオ"
-        />
-      </div>
-    </Layout>
+    <Loading>
+      <Layout title="Home">
+        <div className="flex justify-center items-center w-full bg-blue-200">
+          <img
+            className="animate__animated animate__fadeIn lg:w-1/2"
+            src="/images/portfolio_top.png"
+            alt="ポートフォリオ"
+          />
+        </div>
+      </Layout>
+    </Loading>
   )
 }
 
