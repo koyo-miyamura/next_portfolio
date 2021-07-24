9057f5865c21f5e88af0f00e78eb88e89dbb2897
koyo-miyamura <nexus9071@gmail.com>
2021-07-24 14:42:18 +0900

    名前ミスってた

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
-import { useNProgress } from '../lib/hooks/useNprogress'
+import { useNProgress } from '../lib/hooks/useNProgress'
 
 function MyApp({ Component, pageProps }: AppProps) {
   useNProgress()
