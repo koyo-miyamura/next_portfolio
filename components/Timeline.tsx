86a3ad72fe1c96ddacea4bbda37720ad9505884f
koyo-miyamura <nexus9071@gmail.com>
2021-08-02 02:01:01 +0900

    微調整

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -17,18 +17,18 @@ const Timeline = () => {
           </div>
           <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
             <div className="h-full w-6 flex items-center justify-center">
-              <div className="h-full w-1 bg-sky-800 pointer-events-none" />
+              <div className="h-full w-1 bg-sky-600 pointer-events-none" />
             </div>
-            <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-sky-500 shadow hover:animate-ping" />
+            <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-sky-400 shadow hover:animate-ping border-solid border-sky-200 border-2" />
           </div>
         </div>
         {/* <!-- right --> */}
         <div className="flex md:contents">
           <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
             <div className="h-full w-6 flex items-center justify-center">
-              <div className="h-full w-1 bg-sky-800 pointer-events-none" />
+              <div className="h-full w-1 bg-sky-600 pointer-events-none" />
             </div>
-            <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-sky-500 shadow hover:animate-ping" />
+            <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-sky-400 shadow hover:animate-ping border-solid border-sky-200 border-2" />
           </div>
           <div className="col-start-6 col-end-10 p-4 my-4 shadow-md border-t-4 border-momizi">
             <span className="font-semibold text-xs text-momizi mb-1">2000 - 2010</span>
