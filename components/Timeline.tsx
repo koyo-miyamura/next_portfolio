11a5943debbf26a543b56ce03f3b95ea2cfed332
koyo-miyamura <nexus9071@gmail.com>
2021-08-07 21:38:48 +0900

    ざっくり分割

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,44 +1,62 @@
 import React from 'react'
 
-type Props = {}
+type Props = {
+  timelines: {
+    period: string
+    title: string
+    content: JSX.Element
+  }[]
+}
 
-const Timeline = () => {
+const Timeline = ({ timelines }: Props) => {
+  const Period = ({ period }: { period: string }) => (
+    <span className="font-semibold text-xs text-momizi mb-1">{period}</span>
+  )
+  const Title = ({ title }: { title: string }) => <h3 className="font-semibold text-lg mb-1">{title}</h3>
+  const Content = ({ content }: { content: JSX.Element }) => <p className="leading-tight">{content}</p>
+
+  const LineCircle = () => (
+    <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+      <div className="h-full w-6 flex items-center justify-center">
+        <div className="h-full w-1 bg-sky-600 pointer-events-none" />
+      </div>
+      <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-sky-400 shadow hover:animate-ping border-solid border-sky-200 border-2" />
+    </div>
+  )
+
+  const LeftCard = () => (
+    <div className="flex flex-row-reverse md:contents">
+      <div className="col-start-1 col-end-5 p-4 my-4 shadow-md border-t-4 border-momizi">
+        <Period period="2000-2010" />
+        <Title title="title" />
+        <Content content={<>今日もいい天気</>} />
+      </div>
+      <LineCircle />
+    </div>
+  )
+
+  const RightCard = () => (
+    <div className="flex md:contents">
+      <LineCircle />
+      <div className="col-start-6 col-end-10 p-4 my-4 shadow-md border-t-4 border-momizi">
+        <Period period="2000-2010" />
+        <Title title="title" />
+        <Content content={<>今日もいい天気</>} />
+      </div>
+    </div>
+  )
   return (
     <div className="container">
       <div className="flex flex-col md:grid md:grid-cols-9 p-2">
-        {/* <!-- left --> */}
-        <div className="flex flex-row-reverse md:contents">
-          <div className="col-start-1 col-end-5 p-4 my-4 shadow-md border-t-4 border-momizi">
-            <span className="font-semibold text-xs text-momizi mb-1">2000 - 2010</span>
-            <h3 className="font-semibold text-lg mb-1">タイトル</h3>
-            <p className="leading-tight">
-              今日はいい天気。 今日はいい天気今日はいい天気 今日はいい天気今日はいい天気今日はいい天気
-            </p>
-          </div>
-          <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
-            <div className="h-full w-6 flex items-center justify-center">
-              <div className="h-full w-1 bg-sky-600 pointer-events-none" />
-            </div>
-            <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-sky-400 shadow hover:animate-ping border-solid border-sky-200 border-2" />
-          </div>
-        </div>
-        {/* <!-- right --> */}
-        <div className="flex md:contents">
-          <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-            <div className="h-full w-6 flex items-center justify-center">
-              <div className="h-full w-1 bg-sky-600 pointer-events-none" />
-            </div>
-            <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-sky-400 shadow hover:animate-ping border-solid border-sky-200 border-2" />
-          </div>
-          <div className="col-start-6 col-end-10 p-4 my-4 shadow-md border-t-4 border-momizi">
-            <span className="font-semibold text-xs text-momizi mb-1">2000 - 2010</span>
-            <h3 className="font-semibold text-lg mb-1">Lorem ipsum</h3>
-            <p className="leading-tight text-justify">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, facilis. Lorem ipsum, dolor sit amet
-              consectetur adipisicing elit. Vitae, facilis.
-            </p>
-          </div>
-        </div>
+        {timelines.map(({ period, title, content }, i) => {
+          console.log(period, title, content)
+          if (i % 2 === 0) {
+            return <LeftCard />
+          }
+          return <RightCard />
+        })}
+        <LeftCard />
+        <RightCard />
       </div>
     </div>
   )
