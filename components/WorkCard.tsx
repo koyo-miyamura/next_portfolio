cd7fb5c8f628952b916fdc7deb602283d524d3b5
koyo-miyamura <nexus9071@gmail.com>
2021-08-23 01:39:04 +0900

    Workページのひな型を雑に実装してみる

---
+++ components/WorkCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Props = {
+  title?: string
+  content?: string
+  imageUrl?: string
+}
+
+const WorkCard = ({ title = '', content = '', imageUrl = '' }: Props) => (
+  <div className="lg:m-4 lg:w-1/3 shadow-md hover:shadow-lg hover:bg-gray-100 rounded-lg bg-white my-12 mx-8">
+    <img src={imageUrl} alt="" className="overflow-hidden" />
+    <div className="p-4">
+      <h3 className="font-medium text-gray-600 text-lg my-2">{title}</h3>
+      <p className="text-justify">{content}</p>
+    </div>
+  </div>
+)
+
+export default WorkCard
--- pages/work.tsx
+++ pages/work.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import Layout from 'components/Layout'
+import WorkCard from 'components/WorkCard'
+import LifeGameImage from 'public/images/lifegame.png'
 
 const WorkPage = () => (
   <Layout title="Work">
-    <h1>仕事とプライベートの作品集</h1>
+    <h1 className="text-4xl font-bold text-center p-2">Work</h1>
+    <div className="lg:flex items-center container">
+      <WorkCard
+        imageUrl="https://www.kayac.com/uploads/project/main_image/1188/main."
+        title="ぼくらの甲子園！ポケット"
+        content="Perl5を用いたサーバサイド実装の担当"
+      />
+      <WorkCard
+        imageUrl="https://www.kayac.com/uploads/project/main_image/1682/main."
+        title="進撃の巨人タクティクス"
+        content="Goを用いたサーバサイド実装の担当"
+      />
+    </div>
+    <h1 className="text-4xl font-bold text-center p-2">Hobby</h1>
+    <div className="lg:flex items-center container">
+      <WorkCard imageUrl={LifeGameImage.src} title="LifeGame" content="Reactを用いたライフゲーム" />
+    </div>
   </Layout>
 )
 
