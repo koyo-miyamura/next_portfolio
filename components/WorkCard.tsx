4f1a1ad31dc9126eee15e36b2a4059f33299f0cb
koyo-miyamura <nexus9071@gmail.com>
2021-08-25 18:24:40 +0900

    Workページ全体的に調整

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -4,15 +4,18 @@ type Props = {
   title?: string
   content?: string
   imageUrl?: string
+  href?: string
 }
 
-const WorkCard = ({ title = '', content = '', imageUrl = '' }: Props) => (
-  <div className="lg:m-4 lg:w-1/3 shadow-md hover:shadow-lg hover:bg-gray-100 rounded-lg bg-white my-12 mx-8">
-    <img src={imageUrl} alt="" className="overflow-hidden" />
-    <div className="p-4">
-      <h3 className="font-medium text-gray-600 text-lg my-2">{title}</h3>
-      <p className="text-justify">{content}</p>
-    </div>
+const WorkCard = ({ title = '', content = '', imageUrl = '', href = '' }: Props) => (
+  <div className="lg:m-4 lg:w-80 shadow-md hover:sdow-lg hover:bg-gray-100 rounded-lg bg-white my-12 mx-0 md:mx-8">
+    <a href={href}>
+      <img src={imageUrl} alt="" className="overflow-hidden" />
+      <div className="p-4">
+        <h3 className="font-medium text-gray-600 text-lg my-2">{title}</h3>
+        <p className="text-justify">{content}</p>
+      </div>
+    </a>
   </div>
 )
 
--- pages/work.tsx
+++ pages/work.tsx
@@ -6,23 +6,37 @@ import LGTMakerImage from 'public/images/lgtmaker.jpg'
 
 const WorkPage = () => (
   <Layout title="Work">
-    <h1 className="text-4xl font-bold text-center p-2">Work</h1>
-    <div className="lg:flex items-center container">
-      <WorkCard
-        imageUrl="https://www.kayac.com/uploads/project/main_image/1188/main."
-        title="ぼくらの甲子園！ポケット"
-        content="Perl5を用いたサーバサイド実装の担当"
-      />
-      <WorkCard
-        imageUrl="https://www.kayac.com/uploads/project/main_image/1682/main."
-        title="進撃の巨人タクティクス"
-        content="Goを用いたサーバサイド実装の担当"
-      />
-    </div>
-    <h1 className="text-4xl font-bold text-center p-2">Hobby</h1>
-    <div className="lg:flex items-start container">
-      <WorkCard imageUrl={LifeGameImage.src} title="LifeGame" content="Reactを用いたライフゲーム" />
-      <WorkCard imageUrl={LGTMakerImage.src} title="LGTMaker" content="Reactを用いたLGTM画像生成" />
+    <div className="container px-8">
+      <h1 className="text-4xl font-bold text-center py-4">Work</h1>
+      <div className="lg:flex items-start container flex-wrap justify-around">
+        <WorkCard
+          imageUrl="https://www.kayac.com/uploads/project/main_image/1188/main."
+          title="ぼくらの甲子園！ポケット"
+          content="Perl5を用いたサーバサイド実装の担当"
+          href="https://www.kayac.com/service/game/1188"
+        />
+        <WorkCard
+          imageUrl="https://www.kayac.com/uploads/project/main_image/1682/main."
+          title="進撃の巨人タクティクス"
+          content="Goを用いたサーバサイド実装の担当"
+          href="https://www.kayac.com/service/game/1682"
+        />
+      </div>
+      <h1 className="text-4xl font-bold text-center py-4">Hobby</h1>
+      <div className="lg:flex items-start container flex-wrap justify-around">
+        <WorkCard
+          imageUrl={LifeGameImage.src}
+          title="LifeGame"
+          content="Reactを用いたライフゲーム"
+          href="https://lifegames.netlify.app/"
+        />
+        <WorkCard
+          imageUrl={LGTMakerImage.src}
+          title="LGTMaker"
+          content="Reactを用いたLGTM画像生成"
+          href="https://lgtmaker.netlify.app/"
+        />
+      </div>
     </div>
   </Layout>
 )
